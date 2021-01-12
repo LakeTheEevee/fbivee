0a0971cd5557259dfb335f406b9222d170f1b704
Lake The Eevee <52654313+LakeTheEevee@users.noreply.github.com>
2021-01-12 11:01:11 +0800

    askbiden
    1. askbiden works (needs responses and pics)
    2. reload crashes when i rl something that doesnt exist
    3. prefix - to .

--- commands/askbiden.js
+++ commands/askbiden.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
     const Discord = require("discord.js");
-    var responses = ["Will you shut up, man?", "Folks, do you have any idea what this clown is doing?", "And him, too.", "FLY WILL VOTE"]
-    var pics = ["https://api.time.com/wp-content/uploads/2019/09/joe-biden-ukraine-fundraising.jpg?w=800&quality=85"]
-    const exampleEmbed = new Discord.MessageEmbed()
+    const responses = ["Will you shut up, man?", "Folks, do you have any idea what this clown is doing?", "And him, too.", "FLY WILL VOTE"];
+    const pics = ["https://api.time.com/wp-content/uploads/2019/09/joe-biden-ukraine-fundraising.jpg?w=800&quality=85"];
+    const colours = ["#00300a", "#507248", "#380909", "#2b092a", "#180228"];
+    const text = args.join(" ");
+    const embed = new Discord.MessageEmbed()
     .setTitle('Ask Biden')
-	.setImage(pics[Math.floor(Math.random() * pics.length)]);
-
-    message.channel.send(exampleEmbed);
+    .setDescription(`${message.member.user.username}: ${text}\n\nBiden: ${responses[Math.floor(Math.random() * responses.length)]}`)
+    .setThumbnail(pics[Math.floor(Math.random() * pics.length)])
+    .setColor(colours[Math.floor(Math.random() * colour.length)]);
+    message.channel.send(embed);
 }
--- commands/rl.js
+++ commands/rl.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   const config = require("../config.json");
   if (message.member.id !== config.ownerid) {
     return (
-      message.reply(\`*loads ak\* if you dont stop trying ill shoot you`),
+      message.reply(`\*loads ak\* if you dont stop trying ill shoot you`),
       console.log(
         `${message.member.user.tag} isnt the owner and tried to rl smfh`
       )
@@ -15,14 +15,15 @@ exports.run = (client, message, args) => {
         `hey you give me something real to rl`
       )
     );
+  } else {
+    const commandName = args[0];
+    delete require.cache[require.resolve(`./${commandName}.js`)];
+    client.commands.delete(commandName);
+    const props = require(`./${commandName}.js`);
+    client.commands.set(commandName, props);
+    message.reply(`\*loads gun\* got it`);
+    console.log(
+      `bui bui rl\'ed ${args}`
+    );
   }
-  const commandName = args[0];
-  delete require.cache[require.resolve(`./${commandName}.js`)];
-  client.commands.delete(commandName);
-  const props = require(`./${commandName}.js`);
-  client.commands.set(commandName, props);
-  message.reply(`*loads gun* got it`);
-  console.log(
-    `bui bui rl\'ed ${args}`
-  );
 };
