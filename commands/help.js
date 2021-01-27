1c263925c3b83869e0db2a40805b4eaf2b4db093
Lake The Eevee <52654313+LakeTheEevee@users.noreply.github.com>
2021-01-27 10:19:54 +0800

    fixed askbiden logs and added help

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+    const config = require("../config.json")
+    message.channel.send(`Hi! My Prefix is \`.\`
+    There are currently 5 commands:
+    Useful:
+    - help
+    - ping
+    Fun:
+    - askbiden
+    - echo
+    - nickbet (:construction_worker: :hammer_and_wrench: Work In Progress, Crashes)`)
+    console.log(`${message.member.user.tag} used help`)
+    if(message.author.id == config.ownerid) {
+    message.channel.send (`Hey psst!
+    Admin:
+    - rl`)
+    console.log(`${message.member.user.tag} activated the admin help pog pog pog`)
+    }
+}
