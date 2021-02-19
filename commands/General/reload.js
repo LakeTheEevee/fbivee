4663a42e8597bc67b562414952ea572b9a5e1668
Lake The Eevee <52654313+LakeTheEevee@users.noreply.github.com>
2021-02-19 16:58:25 +0800

    2cringe4u

--- commands/General/reload.js
+++ commands/General/reload.js
@@ -1,27 +1,14 @@
 module.exports.run = async (client, message, args) => {
   const commandName = args[0];
   const config = require("../../config.js");
-  if (message.member.id !== config.OWNERID) {
-    return (
-      message.reply(`\*loads ak\* if you dont stop trying ill shoot you`),
-      console.log(
-        `${message.member.user.tag} isnt the owner and tried to rl smfh`
-      )
-    );
-  }
+  if (message.member.id !== config.OWNERID) return
   if (!args || args.length < 1) {
     return (
-      message.reply(`fr? \*loads glock\*`),
-      console.log(
-        `${message.member.user.tag} used rl but didnt give a command smfh`
-      )
+      message.reply(`fr?`)
     );
   } else if (!client.commands.has(commandName)) {
     return (
-      message.reply("are you stupid or are you stupid"),
-      console.log(
-        `${message.member.user.tag} used rl but didnt give a command smfh`
-      )
+      message.reply("are you stupid or are you stupid")
     );
   } else {
     const commandName = args[0];
@@ -29,8 +16,7 @@ module.exports.run = async (client, message, args) => {
     client.commands.delete(commandName);
     const props = require(`./${commandName}.js`);
     client.commands.set(commandName, props);
-    message.reply(`\*loads gun\* got it`);
-    console.log(`bui bui rl\'ed ${args}`);
+    message.reply(`got it`);
   }
 };
 
