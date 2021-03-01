22ec981ba941cd270f4b86bdde4ce68aa86e5219
Lake <lake@inkthought.codes>
2021-03-01 13:24:32 +0800

    readying for actual .example files

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN: process.env.TOKEN,
-    PREFIX: process.env.PREFIX,
-    OWNERID: "441912852710948864",
-    DBURL: `mongodb+srv://${process.env.DBUN}:${process.env.DBPW}@${process.env.DBSURL}/${process.env.DBNAME}?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`,
-};
